7182ace2621be41c80c681a66f81921663ae9edd
Johan Segerfeldt <johan.segerfeldt@me.com>
2021-08-15 23:35:19 +0200

    Add documentation

--- Segerfeldt.EventStore.Source/PublishedEvent.cs
+++ Segerfeldt.EventStore.Source/PublishedEvent.cs
@@ -2,11 +2,16 @@ using System;
 
 namespace Segerfeldt.EventStore.Source
 {
+    /// <summary>An event that has been published and is part of the official state of an entity</summary>
     public class PublishedEvent
     {
+        /// <summary>A name identifying what aspect of the entity changed</summary>
         public string Name { get; }
+        /// <summary>A JSON-serialized object that specifies the details of the change</summary>
         public string Details { get; }
+        /// <summary>The user that published this event</summary>
         public string Actor { get; }
+        /// <summary>The instant this event was published</summary>
         public DateTime Timestamp { get; }
 
         public PublishedEvent(string name, string details, string actor, DateTime timestamp)
--- Segerfeldt.EventStore.Source/UnpublishedEvent.cs
+++ Segerfeldt.EventStore.Source/UnpublishedEvent.cs
@@ -1,14 +1,42 @@
 namespace Segerfeldt.EventStore.Source
 {
+    /// <summary>An event that hasn't been published yet</summary>
+    /// The event should include all the details needed to unambiguously
+    /// define the new state of an entity (given an unambiguous prior state).
+    /// <example>
+    /// A Student entity might enroll in a course:
+    /// <code>
+    /// {
+    ///   Name = "Enrolled";
+    ///   Details = {CourseId = "CS193p"}
+    /// }
+    /// </code>
+    /// </example>
     public sealed class UnpublishedEvent
     {
+        /// <summary>What aspect of the entity changes</summary>
+        /// <example><c>"Enrolled"</c></example>
+        /// The name defines an aspect of the entity that should change.
+        /// This is the typical property to filter on when
+        /// selecting what state information to project.
         public string Name { get; }
+
+        /// <summary>Details regarding the change</summary>
+        /// <example><c>new {CourseId = "CS193p"}</c></example>
         public object Details { get; }
 
+        /// <param name="name">what aspect of the entity is changing</param>
+        /// <param name="details">details regarding the change</param>
+        /// <example><c>
+        /// new (
+        ///   name: "Enrolled",
+        ///   details: new {CourseId = "CS193p"}
+        /// )
+        /// </c></example>
         public UnpublishedEvent(string name, object details)
         {
-            Details = details;
             Name = name;
+            Details = details;
         }
     }
 }
