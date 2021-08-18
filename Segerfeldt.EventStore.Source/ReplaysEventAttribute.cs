b552ed434c03492748e6c5021247af521b4e18b2
Johan Segerfeldt <johan.segerfeldt@me.com>
2021-08-18 21:33:51 +0200

    Fix warnings

--- Segerfeldt.EventStore.Source/ReplaysEventAttribute.cs
+++ Segerfeldt.EventStore.Source/ReplaysEventAttribute.cs
@@ -1,8 +1,11 @@
+using JetBrains.Annotations;
+
 using System;
 
 namespace Segerfeldt.EventStore.Source
 {
     [AttributeUsage(AttributeTargets.Method)]
+    [MeansImplicitUse]
     public class ReplaysEventAttribute : Attribute
     {
         public string Event { get; }
--- Tests/Segerfeldt.EventStore.Projection.Tests/SQLServerProjectionTests.cs
+++ Tests/Segerfeldt.EventStore.Projection.Tests/SQLServerProjectionTests.cs
@@ -2,7 +2,6 @@ using Moq;
 
 using NUnit.Framework;
 
-using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
