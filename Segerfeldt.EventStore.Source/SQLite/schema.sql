4af84a0eeb03b11646fa0c5c141823be168d0da3
Johan Segerfeldt <johan.segerfeldt@me.com>
2021-09-03 17:25:00 +0200

    Add default value to timestamp

--- Segerfeldt.EventStore.Source/SQLite/schema.sql
+++ Segerfeldt.EventStore.Source/SQLite/schema.sql
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS Events (
     name TEXT,
     details TEXT,
     actor TEXT,
-    timestamp TIMESTAMP,
+    timestamp TIMESTAMP default current_timestamp,
     version INT,
     position BIGINT,
 
