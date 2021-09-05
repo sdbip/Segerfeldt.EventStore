5967c522b70571c7360ddb906009bed270fd4dd1
Johan Segerfeldt <johan.segerfeldt@me.com>
2021-09-05 08:06:27 +0200

    Use AddSingleton<IHostedService>() to allow multiple instances

--- Segerfeldt.EventStore.Projection/Hosting/ServiceCollectionExtension.cs
+++ Segerfeldt.EventStore.Projection/Hosting/ServiceCollectionExtension.cs
@@ -1,4 +1,5 @@
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace Segerfeldt.EventStore.Projection.Hosting
 {
@@ -6,8 +7,14 @@ namespace Segerfeldt.EventStore.Projection.Hosting
     {
         public static EventSourceBuilder AddHostedEventSource(this IServiceCollection services, IConnectionPool connectionPool)
         {
+            // Note: AddHostedService<T>() will only add one service per unique type T. Even if called
+            // multiple times. If the user needs to track more than one Source, we'd need a new
+            // HostedEventSource *class* for each one. Fortunately, AddSingleton<IHostedService>() does
+            // not have such restrictions. And all IHostedServices added *will* be started by the .Net
+            // Web API system.
+
             var builder = new EventSourceBuilder(connectionPool);
-            services.AddHostedService(p => new HostedEventSource(builder.Build(p)));
+            services.AddSingleton<IHostedService>(p => new HostedEventSource(builder.Build(p)));
             return builder;
         }
     }
