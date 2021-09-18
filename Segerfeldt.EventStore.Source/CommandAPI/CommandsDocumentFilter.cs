58498db3bb191f18f21522558a4a41c4506b2cbb
Johan Segerfeldt <johan.segerfeldt@me.com>
2021-09-18 07:25:05 +0200

    Extract smaller methods

--- Segerfeldt.EventStore.Source/CommandAPI/CommandsDocumentFilter.cs
+++ Segerfeldt.EventStore.Source/CommandAPI/CommandsDocumentFilter.cs
@@ -18,42 +18,51 @@ namespace Segerfeldt.EventStore.Source.CommandAPI
 
         public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
         {
-            var handlerTypes = assemblies
-                .SelectMany(assembly => assembly
-                    .GetExportedTypes()
-                    .Where(type => type.IsClass)
-                    .Where(type => !type.IsAbstract)
-                    .Where(type => type.GetCustomAttribute<ModifiesEntityAttribute>() is not null))
-                .GroupBy(type => type.GetCustomAttribute<ModifiesEntityAttribute>(false)!.Pattern);
+            var pathItemsByPattern = FindCommandHandlerTypes()
+                .GroupBy(handlerType => handlerType.CommandAttribute.Pattern)
+                .Select(group => (group.Key, CreateOpenApiPathItem(group, context)));
 
-            foreach (var group in handlerTypes)
-            {
-                var pathItem = new OpenApiPathItem();
-                foreach (var handlerType in group)
-                {
-                    var attribute = handlerType.GetCustomAttribute<ModifiesEntityAttribute>(false)!;
-                    var operation = CreateOpenApiOperation(handlerType, attribute, context);
-                    if (operation is null) continue;
-
-                    pathItem.Operations.Add(attribute.Method, operation);
-                }
-                swaggerDoc.Paths.Add(group.Key, pathItem);
-            }
+            foreach (var (pattern, item) in pathItemsByPattern)
+                swaggerDoc.Paths.Add(pattern, item);
         }
 
-        private static OpenApiOperation? CreateOpenApiOperation(Type handlerType, ModifiesEntityAttribute attribute, DocumentFilterContext context)
-        {
-            var commandHandlerType = GetCommandHandlerType(handlerType);
-            if (commandHandlerType is null) return null;
+        private IEnumerable<(Type type, ModifiesEntityAttribute CommandAttribute)> FindCommandHandlerTypes() =>
+            assemblies
+                .SelectMany(assembly => assembly.GetExportedTypes()
+                    .Where(type => type.IsClass && !type.IsAbstract)
+                    .Select(type =>
+                    {
+                        var modifiesEntityAttribute = type.GetCustomAttribute<ModifiesEntityAttribute>(false);
+                        if (modifiesEntityAttribute is null) return ((Type, ModifiesEntityAttribute CommandAttribute)?)null;
+                        return (type, CommandAttribute: modifiesEntityAttribute);
+                    })
+                    .RemoveNulls());
 
+        private static OpenApiPathItem CreateOpenApiPathItem(IEnumerable<(Type, ModifiesEntityAttribute)> handlerTypes, DocumentFilterContext context)
+        {
+            var operations = handlerTypes.Select(handlerType =>
+                {
+                    var (type, commandAttribute) = handlerType;
+                    var commandHandlerType = GetCommandHandlerType(type);
+                    if (commandHandlerType is null) return null;
+
+                    var operation = CreateOpenApiOperation(commandAttribute, context, commandHandlerType, type.Name);
+                    return ((OperationType method, OpenApiOperation operation)?)(commandAttribute.Method, operation);
+                })
+                .RemoveNulls()
+                .Select(tuple => new KeyValuePair<OperationType, OpenApiOperation>(tuple.method, tuple.operation));
+
+            return new OpenApiPathItem { Operations = new Dictionary<OperationType, OpenApiOperation>(operations) };
+        }
+
+        private static OpenApiOperation CreateOpenApiOperation(ModifiesEntityAttribute attribute, DocumentFilterContext context,
+            Type commandHandlerType, string operationId)
+        {
             var genericArguments = commandHandlerType.GetGenericArguments();
             var commandType = genericArguments[0];
             var dtoType = genericArguments.Skip(1).FirstOrDefault();
 
             var requestSchema = context.SchemaGenerator.GenerateSchema(commandType, context.SchemaRepository);
-            var responseSchema = dtoType is null
-                ? new OpenApiSchema()
-                : context.SchemaGenerator.GenerateSchema(dtoType, context.SchemaRepository);
 
             if (!context.SchemaRepository.Schemas.TryGetValue(commandType.FullName!, out var commandSchema))
                 context.SchemaRepository.Schemas.TryGetValue(commandType.Name, out commandSchema);
@@ -61,21 +70,9 @@ namespace Segerfeldt.EventStore.Source.CommandAPI
             var operation = new OpenApiOperation
             {
                 Summary = commandSchema?.Description,
-                OperationId = handlerType.Name,
-                Responses = new OpenApiResponses
-                {
-                    ["200"] = new()
-                    {
-                        Content =
-                        {
-                            ["application/json"] = new OpenApiMediaType { Schema = responseSchema, }
-                        }
-                    }
-                },
-                Tags =
-                {
-                    new OpenApiTag { Name = attribute.Entity }
-                }
+                OperationId = operationId,
+                Responses = new OpenApiResponses { ["200"] = CreateResponseItem(dtoType, context) },
+                Tags = { new OpenApiTag { Name = attribute.Entity } }
             };
 
             switch (attribute.Method)
@@ -83,17 +80,7 @@ namespace Segerfeldt.EventStore.Source.CommandAPI
                 case OperationType.Get:
                 {
                     foreach (var property in commandType.GetProperties())
-                    {
-                        operation.Parameters.Add(
-                            new OpenApiParameter
-                            {
-                                Name = property.Name,
-                                In = ParameterLocation.Query,
-                                Schema = new OpenApiSchema { Type = "string" },
-                                Description = commandSchema?.Properties.FirstOrDefault(p => string.Equals(p.Key, property.Name, StringComparison.InvariantCultureIgnoreCase)).Value?.Description
-                            });
-                    }
-
+                        operation.Parameters.Add(CreateOpenApiParameter(property.Name, commandSchema!));
                     break;
                 }
                 case OperationType.Post or OperationType.Put or OperationType.Patch:
@@ -105,28 +92,9 @@ namespace Segerfeldt.EventStore.Source.CommandAPI
             }
 
             if (attribute.Method == OperationType.Delete)
-            {
-                operation.Parameters.Add(
-                    new OpenApiParameter
-                    {
-                        Name = attribute.EntityIdOrDefault,
-                        In = ParameterLocation.Path,
-                        Schema = new OpenApiSchema { Type = "string" },
-                        Description = $"the entity id of the {attribute.Entity} to delete"
-                    });
-            }
-
+                operation.Parameters.Add(CreateEntityIdParameterItem(attribute, "remove"));
             else if (attribute.Property is not null)
-            {
-                operation.Parameters.Add(
-                    new OpenApiParameter
-                    {
-                        Name = attribute.EntityIdOrDefault,
-                        In = ParameterLocation.Path,
-                        Schema = new OpenApiSchema { Type = "string" },
-                        Description = $"the entity id of the {attribute.Entity} to modify"
-                    });
-            }
+                operation.Parameters.Add(CreateEntityIdParameterItem(attribute, "modify"));
 
             return operation;
         }
@@ -143,6 +111,34 @@ namespace Segerfeldt.EventStore.Source.CommandAPI
 
             return null;
         }
+
+        private static OpenApiResponse CreateResponseItem(Type? dtoType, DocumentFilterContext context)
+        {
+            var schema = dtoType is null
+                ? new OpenApiSchema()
+                : context.SchemaGenerator.GenerateSchema(dtoType, context.SchemaRepository);
+            return new OpenApiResponse { Content = { ["application/json"] = new OpenApiMediaType { Schema = schema } } };
+        }
+
+        private static OpenApiParameter CreateOpenApiParameter(string propertyName, OpenApiSchema commandSchema) =>
+            new()
+            {
+                Name = propertyName,
+                In = ParameterLocation.Query,
+                Schema = new OpenApiSchema { Type = "string" },
+                Description = commandSchema.Properties
+                    .FirstOrDefault(p => string.Equals(p.Key, propertyName, StringComparison.InvariantCultureIgnoreCase))
+                    .Value?.Description
+            };
+
+        private static OpenApiParameter CreateEntityIdParameterItem(ModifiesEntityAttribute attribute, string modificationString) =>
+            new()
+            {
+                Name = attribute.EntityIdOrDefault,
+                In = ParameterLocation.Path,
+                Schema = new OpenApiSchema { Type = "string" },
+                Description = $"the entity id of the {attribute.Entity} to {modificationString}"
+            };
 
         private static bool IsCommandHandlerInterface(Type t) =>
             t.IsGenericType &&
---
+++ Segerfeldt.EventStore.Source/CommandAPI/RemoveNullsExtension.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Segerfeldt.EventStore.Source.CommandAPI
+{
+    internal static class RemoveNullsExtension
+    {
+        public static IEnumerable<T> RemoveNulls<T>(this IEnumerable<T?> list) where T : class =>
+            list.Where(item => item is not null).Select(item => item!);
+        public static IEnumerable<T> RemoveNulls<T>(this IEnumerable<T?> list) where T : struct =>
+            list.Where(item => item is not null).Select(item => item!.Value);
+    }
+}
