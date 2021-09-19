df1c0168290871c341d9a84f7030f63e5b92511b
Johan Segerfeldt <johan.segerfeldt@me.com>
2021-09-19 07:16:34 +0200

    Move methods to extensions

--- Segerfeldt.EventStore.Source/EntityStore.cs
+++ Segerfeldt.EventStore.Source/EntityStore.cs
@@ -1,146 +1,42 @@
 ﻿using Segerfeldt.EventStore.Source.Internals;
-using Segerfeldt.EventStore.Source.Snapshots;
 
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Segerfeldt.EventStore.Source
 {
+    /// <summary>An object that represents the “source of truth” write model of an event-sourced CQRS architecture</summary>
     public interface IEntityStore
     {
-        /// <summary>Reconstitute the state of an entity from published events</summary>
-        /// <param name="id">the unique identifier of the entity to reconstitute</param>
-        /// <param name="type"></param>
-        /// <param name="cancellationToken"></param>
-        /// <typeparam name="TEntity">the type of the entity</typeparam>
-        /// <returns>the entity with the specified <paramref name="id"/></returns>
-        Task<TEntity?> ReconstituteAsync<TEntity>(EntityId id, EntityType type, CancellationToken cancellationToken = default) where TEntity : class, IEntity;
-
-        /// <summary>Get the historical data about an entity</summary>
-        /// <param name="entityId">the unique identifier of the entity to reconstitute</param>
+        /// <summary>Finds all the events, and the current version, of an entity. Everything needed to reconstitute its state.</summary>
+        /// <param name="entityId">the id of the entity</param>
+        /// <param name="afterVersion">only events that occurred after this version (and excluding this version)  will be returned. useful if you have a snapshot.</param>
         /// <param name="cancellationToken"></param>
         /// <returns>the complete history of the entity</returns>
-        Task<EntityHistory?> GetHistoryAsync(EntityId entityId, CancellationToken cancellationToken = default);
+        Task<EntityHistory?> GetHistoryAsync(EntityId entityId, EntityVersion afterVersion, CancellationToken cancellationToken = default);
+
+        /// <summary>Looks up the type of an entity. Useful for quickly checking if an entity id is taken.</summary>
+        /// <param name="entityId">the id to verify</param>
+        /// <param name="cancellationToken"></param>
+        /// <returns>true if there is an entity with the given id, false otherwise</returns>
+        Task<EntityType?> GetEntityTypeAsync(EntityId entityId, CancellationToken cancellationToken = default);
     }
 
-    /// <summary>An object that represents the “source of truth” write model of an event-sourced CQRS architecture</summary>
+    /// <inheritdoc />
     public sealed class EntityStore : IEntityStore
     {
         private readonly IConnectionPool connectionPool;
 
         /// <summary>Initialize a new <see cref="EntityStore"/></summary>
         /// <param name="connectionPool">opens connections to the database that stores the state of entities as sequences of events</param>
-        public EntityStore(IConnectionPool connectionPool)
-        {
-            this.connectionPool = connectionPool;
-        }
+        public EntityStore(IConnectionPool connectionPool) => this.connectionPool = connectionPool;
 
-        /// <summary>Reconstitute the state of an entity from published events</summary>
-        /// <param name="id">the unique identifier of the entity to reconstitute</param>
-        /// <param name="type"></param>
-        /// <typeparam name="TEntity">the type of the entity</typeparam>
-        /// <returns>the entity with the specified <paramref name="id"/></returns>
-        public TEntity? Reconstitute<TEntity>(EntityId id, EntityType type) where TEntity : class, IEntity =>
-            ReconstituteAsync<TEntity>(id, type).Result;
-
-        /// <summary>Reconstitute the state of an entity from published events</summary>
-        /// <param name="id">the unique identifier of the entity to reconstitute</param>
-        /// <param name="type"></param>
-        /// <param name="cancellationToken"></param>
-        /// <typeparam name="TEntity">the type of the entity</typeparam>
-        /// <returns>the entity with the specified <paramref name="id"/></returns>
-        public async Task<TEntity?> ReconstituteAsync<TEntity>(EntityId id, EntityType type, CancellationToken cancellationToken = default) where TEntity : class, IEntity =>
-            await ReconstituteAsync(new NeverSnapshot<TEntity>(id, type), cancellationToken);
-
-        /// <summary>Reconstitute the state of an entity from published events</summary>
-        /// <param name="snapshot">the snapshot of the entity</param>
-        /// <typeparam name="TEntity">the type of the entity</typeparam>
-        public TEntity? Reconstitute<TEntity>(ISnapshot<TEntity> snapshot) where TEntity : class, IEntity =>
-            ReconstituteAsync(snapshot).Result;
-
-        /// <summary>Reconstitute the state of an entity from published events</summary>
-        /// <param name="snapshot">the snapshot of the entity</param>
-        /// <param name="cancellationToken"></param>
-        /// <typeparam name="TEntity">the type of the entity</typeparam>
-        public async Task<TEntity?> ReconstituteAsync<TEntity>(ISnapshot<TEntity> snapshot, CancellationToken cancellationToken = default) where TEntity : class, IEntity
-        {
-            var history = await GetHistoryAsync(snapshot.Id, snapshot.Version, cancellationToken);
-            if (history is null) return snapshot.Version.IsNew ? null : throw new UnknownEntityException(snapshot.Id);
-            if (history.Type != snapshot.EntityType) throw new IncorrectTypeException(snapshot.EntityType, history.Type);
-            return RestoreEntity(snapshot, history);
-        }
-
-        /// <summary>Get the historical data about an entity</summary>
-        /// <param name="entityId">the unique identifier of the entity to reconstitute</param>
-        /// <returns>the complete history of the entity</returns>
-        public EntityHistory? GetHistory(EntityId entityId) =>
-            GetHistoryAsync(entityId).Result;
-
-        /// <summary>Get the historical data about an entity</summary>
-        /// <param name="entityId">the unique identifier of the entity to reconstitute</param>
-        /// <param name="cancellationToken"></param>
-        /// <returns>the complete history of the entity</returns>
-        public async Task<EntityHistory?> GetHistoryAsync(EntityId entityId, CancellationToken cancellationToken = default) =>
-            await GetHistoryAsync(entityId, EntityVersion.Beginning, cancellationToken);
-
-        private async Task<EntityHistory?> GetHistoryAsync(EntityId entityId, EntityVersion afterVersion, CancellationToken cancellationToken) =>
+        /// <inheritdoc />
+        public async Task<EntityHistory?> GetHistoryAsync(EntityId entityId, EntityVersion afterVersion, CancellationToken cancellationToken) =>
             await new GetHistoryOperation(entityId, afterVersion).ExecuteAsync(connectionPool.CreateConnection(), cancellationToken);
 
-        /// <summary>Looks up the type of an entity. Useful for quickly checking if an entity id is taken.</summary>
-        /// <param name="entityId">the id to verify</param>
-        /// <returns>true if there is an entity with the given id, false otherwise</returns>
-        public bool ContainsEntity(EntityId entityId) => ContainsEntityAsync(entityId).Result;
-
-        /// <summary>Looks up the type of an entity. Useful for quickly checking if an entity id is taken.</summary>
-        /// <param name="entityId">the id to verify</param>
-        /// <param name="cancellationToken"></param>
-        /// <returns>true if there is an entity with the given id, false otherwise</returns>
-        public async Task<bool> ContainsEntityAsync(EntityId entityId, CancellationToken cancellationToken = default) =>
-            await GetEntityTypeAsync(entityId, cancellationToken) is not null;
-
-        /// <summary>Looks up the type of an entity. Useful for quickly checking if an entity id is taken.</summary>
-        /// <param name="entityId">the id to verify</param>
-        /// <returns>true if there is an entity with the given id, false otherwise</returns>
-        public EntityType? GetEntityType(EntityId entityId) => GetEntityTypeAsync(entityId).Result;
-
-        /// <summary>Looks up the type of an entity. Useful for quickly checking if an entity id is taken.</summary>
-        /// <param name="entityId">the id to verify</param>
-        /// <param name="cancellationToken"></param>
-        /// <returns>true if there is an entity with the given id, false otherwise</returns>
+        /// <inheritdoc />
         public async Task<EntityType?> GetEntityTypeAsync(EntityId entityId, CancellationToken cancellationToken = default) =>
             await new LookupEntityTypeOperation(entityId).ExecuteAsync(connectionPool.CreateConnection(), cancellationToken);
-
-        private TEntity RestoreEntity<TEntity>(ISnapshot<TEntity> snapshot, EntityHistory history) where TEntity : class, IEntity
-        {
-            var entity = Instantiate<TEntity>(snapshot.Id, history.Version);
-            snapshot.Restore(entity);
-            entity.ReplayEvents(history.Events);
-            return entity;
-        }
-
-        private TEntity Instantiate<TEntity>(EntityId id, EntityVersion version) where TEntity : IEntity
-        {
-            var constructor = typeof(TEntity).GetConstructor(new[] { typeof(EntityId), typeof(EntityVersion) }) ??
-                              typeof(TEntity).GetConstructor(new[] { typeof(EntityId), typeof(EntityVersion), typeof(EntityStore) });
-            if (constructor is null) throw new InvalidEntityException(typeof(TEntity));
-            return (TEntity)constructor.Invoke(constructor.GetParameters().Length == 2 ? new object[] { id, version } : new object[] { id, version, this });
-        }
-
-        /// <summary>An entity snapshot that was never made.</summary>
-        /// All events will have to be replayed to reconstitute from this snapshot.
-        private class NeverSnapshot<TEntity> : ISnapshot<TEntity> where TEntity : class, IEntity
-        {
-            public EntityId Id { get; }
-            public EntityType EntityType { get; }
-            public EntityVersion Version => EntityVersion.Beginning;
-
-            public NeverSnapshot(EntityId id, EntityType entityType)
-            {
-                Id = id;
-                EntityType = entityType;
-            }
-
-            public void Restore(TEntity entity) { } // Intentionally does nothing
-        }
     }
 }
--- Segerfeldt.EventStore.Source/EventPublisher.cs
+++ Segerfeldt.EventStore.Source/EventPublisher.cs
@@ -6,60 +6,38 @@ using System.Threading.Tasks;
 namespace Segerfeldt.EventStore.Source
 {
     /// <summary>An object that represents the “source of truth” write model of an event-sourced CQRS architecture</summary>
-    public sealed class EventPublisher
+    public interface IEventPublisher
     {
-        private readonly IConnectionPool connectionPool;
-
-        /// <summary>Initialize a new <see cref="EventPublisher"/></summary>
-        /// <param name="connectionPool">opens connections to the database that stores the state of entities as sequences of events</param>
-        public EventPublisher(IConnectionPool connectionPool)
-        {
-            this.connectionPool = connectionPool;
-        }
-
-        /// <summary>Publish a single event for an entity</summary>
-        /// <param name="entityId">the unique identifier for this entity</param>
-        /// <param name="type">the type of the entity if it needs to be created</param>
-        /// <param name="event">the event to publish</param>
-        /// <param name="actor">the actor/user who caused this change</param>
-        public StreamPosition Publish(EntityId entityId, EntityType type, UnpublishedEvent @event, string actor) =>
-            PublishAsync(entityId, type, @event, actor).Result;
+        /// <summary>Publish all new changes since reconstituting an entity</summary>
+        /// <param name="entities">the entities whose events to publish</param>
+        /// <param name="actor">the actor/user who caused these changes</param>
+        Task<StreamPositions> PublishChangesAsync(IEnumerable<IEntity> entities, string actor);
 
         /// <summary>Publish a single event for an entity</summary>
         /// <param name="entityId">the unique identifier for this entity</param>
         /// <param name="type">the type of the entity if it has to be created</param>
         /// <param name="event">the event to publish</param>
         /// <param name="actor">the actor/user who caused this change</param>
+        Task<StreamPosition> PublishAsync(EntityId entityId, EntityType type, UnpublishedEvent @event, string actor);
+    }
+
+    /// <inheritdoc />
+    public sealed class EventPublisher : IEventPublisher
+    {
+        private readonly IConnectionPool connectionPool;
+
+        /// <summary>Initialize a new <see cref="EventPublisher"/></summary>
+        /// <param name="connectionPool">opens connections to the database that stores the state of entities as sequences of events</param>
+        public EventPublisher(IConnectionPool connectionPool) => this.connectionPool = connectionPool;
+
+        /// <inhericdoc />
         public async Task<StreamPosition> PublishAsync(EntityId entityId, EntityType type, UnpublishedEvent @event, string actor)
         {
             var operation = new InsertSingleEventOperation(@event, entityId, type, actor);
             return await operation.ExecuteAsync(connectionPool.CreateConnection());
         }
 
-        /// <summary>Publish all new changes since reconstituting an entity</summary>
-        /// <param name="entity">the entity whose events to publish</param>
-        /// <param name="actor">the actor/user who caused these changes</param>
-        public StreamPosition PublishChanges(IEntity entity, string actor) =>
-            PublishChangesAsync(entity, actor).Result;
-
-        /// <summary>Publish all new changes since reconstituting an entity</summary>
-        /// <param name="entity">the entity whose events to publish</param>
-        /// <param name="actor">the actor/user who caused these changes</param>
-        public async Task<StreamPosition> PublishChangesAsync(IEntity entity, string actor)
-        {
-            var streamPositions = await PublishChangesAsync(new[] { entity }, actor);
-            return new StreamPosition(streamPositions.StorePosition, streamPositions.GetVersion(entity.Id));
-        }
-
-        /// <summary>Publish all new changes since reconstituting an entity</summary>
-        /// <param name="entities">the entity whose events to publish</param>
-        /// <param name="actor">the actor/user who caused these changes</param>
-        public StreamPositions PublishChanges(IEnumerable<IEntity> entities, string actor) =>
-            PublishChangesAsync(entities, actor).Result;
-
-        /// <summary>Publish all new changes since reconstituting an entity</summary>
-        /// <param name="entities">the entities whose events to publish</param>
-        /// <param name="actor">the actor/user who caused these changes</param>
+        /// <inhericdoc />
         public async Task<StreamPositions> PublishChangesAsync(IEnumerable<IEntity> entities, string actor)
         {
             var operation = new InsertEventsOperation(entities, actor);
