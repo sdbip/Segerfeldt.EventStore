b1e09300eefa21ddf68418ebe3cd286e774e2024
Johan Segerfeldt <johan.segerfeldt@me.com>
2021-09-19 20:27:47 +0200

    Add base interface ICommandHandler

--- Segerfeldt.EventStore.Source/CommandAPI/ICommandHandler.cs
+++ Segerfeldt.EventStore.Source/CommandAPI/ICommandHandler.cs
@@ -6,13 +6,15 @@ namespace Segerfeldt.EventStore.Source.CommandAPI
 {
     public record EmptyCommand;
 
-    public interface ICommandHandler<in TCommand>
+    public interface ICommandHandler { }
+
+    public interface ICommandHandler<in TCommand> : ICommandHandler
     {
         public Task<ActionResult> Handle(TCommand command, CommandContext context);
     }
 
-    public interface ICommandHandler<in TCommand, TDTO>
+    public interface ICommandHandler<in TCommand, TResponseDTO> : ICommandHandler
     {
-        public Task<ActionResult<TDTO>> Handle(TCommand command, CommandContext context);
+        public Task<ActionResult<TResponseDTO>> Handle(TCommand command, CommandContext context);
     }
 }
