48743952a28eb412556bd55735666ff7b08a06d2
Johan Segerfeldt <johan.segerfeldt@me.com>
2021-10-02 11:39:43 +0200

    Move class to tests

--- Tests/Segerfeldt.EventStore.Projection.Tests/DelegateReceptacle.cs
+++ Tests/Segerfeldt.EventStore.Projection.Tests/DelegateReceptacle.cs
@@ -2,10 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace Segerfeldt.EventStore.Projection
+namespace Segerfeldt.EventStore.Projection.Tests
 {
-    public delegate Task AsyncSynchronizerDelegate(Event @event);
-    public delegate void SynchronizerDelegate(Event @event);
+    public delegate Task AsyncProjectionDelegate(Event @event);
+    public delegate void ProjectionDelegate(Event @event);
 
     /// <summary><see cref="IReceptacle"/> implementation that uses a delegate</summary>
     public sealed class DelegateReceptacle : IReceptacle
@@ -17,12 +17,12 @@ namespace Segerfeldt.EventStore.Projection
         /// <summary>Initialize a new <see cref="DelegateReceptacle"/></summary>
         /// <param name="delegate">the delegate to call when events are notified</param>
         /// <param name="handledEvents">the events this delegate handles</param>
-        public DelegateReceptacle(AsyncSynchronizerDelegate @delegate, params string[] handledEvents) : this(@delegate, (IEnumerable<string>)handledEvents) { }
+        public DelegateReceptacle(AsyncProjectionDelegate @delegate, params string[] handledEvents) : this(@delegate, (IEnumerable<string>)handledEvents) { }
 
         /// <summary>Initialize a new <see cref="DelegateReceptacle"/></summary>
         /// <param name="delegate">the delegate to call when events are notified</param>
         /// <param name="handledEvents">the events this delegate handles</param>
-        public DelegateReceptacle(AsyncSynchronizerDelegate @delegate, IEnumerable<string> handledEvents)
+        public DelegateReceptacle(AsyncProjectionDelegate @delegate, IEnumerable<string> handledEvents)
         {
             AcceptedEvents = handledEvents;
             this.@delegate = @delegate;
@@ -31,12 +31,12 @@ namespace Segerfeldt.EventStore.Projection
         /// <summary>Initialize a new <see cref="DelegateReceptacle"/></summary>
         /// <param name="delegate">the delegate to call when events are notified</param>
         /// <param name="handledEvents">the events this delegate handles</param>
-        public DelegateReceptacle(SynchronizerDelegate @delegate, params string[] handledEvents) : this(@delegate, (IEnumerable<string>)handledEvents) { }
+        public DelegateReceptacle(ProjectionDelegate @delegate, params string[] handledEvents) : this(@delegate, (IEnumerable<string>)handledEvents) { }
 
         /// <summary>Initialize a new <see cref="DelegateReceptacle"/></summary>
         /// <param name="delegate">the delegate to call when events are notified</param>
         /// <param name="handledEvents">the events this delegate handles</param>
-        public DelegateReceptacle(SynchronizerDelegate @delegate, IEnumerable<string> handledEvents)
+        public DelegateReceptacle(ProjectionDelegate @delegate, IEnumerable<string> handledEvents)
         {
             AcceptedEvents = handledEvents;
             this.@delegate = @delegate;
