7104506110c9f5ac562fb1a3e4021d96845f1656
Johan Segerfeldt <johan.segerfeldt@me.com>
2022-05-20 06:15:40 +0200

    stuff

--- Tests/Segerfeldt.EventStore.Source.Tests/SQLServerReconstitutionTests.cs
+++ Tests/Segerfeldt.EventStore.Source.Tests/SQLServerReconstitutionTests.cs
@@ -8,7 +8,6 @@ using System.Linq;
 
 namespace Segerfeldt.EventStore.Source.Tests;
 
-// ReSharper disable once InconsistentNaming
 public class SQLServerReconstitutionTests
 {
     private SqlConnection connection = null!;
