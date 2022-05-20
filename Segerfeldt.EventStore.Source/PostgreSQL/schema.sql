f452dd6a58149cdbb37158ff63ce74cc9a56470d
Johan Segerfeldt <johan.segerfeldt@me.com>
2022-05-20 06:25:02 +0200

    Fix the schema

--- Segerfeldt.EventStore.Source/PostgreSQL/schema.sql
+++ Segerfeldt.EventStore.Source/PostgreSQL/schema.sql
@@ -9,7 +9,8 @@ CREATE TABLE IF NOT EXISTS Events (
     "name" TEXT NOT NULL,
     "details" TEXT NOT NULL,
     "actor" TEXT NOT NULL,
-    "timestamp" REAL NOT NULL DEFAULT (extract(julian from current_timestamp at time zone 'UTC')),
+    "timestamp" TIMESTAMP WITHOUT TIME ZONE DEFAULT (NOW() AT TIME ZONE 'utc'),
+--    "timestamp" REAL NOT NULL DEFAULT (extract(julian from current_timestamp at time zone 'UTC')),
     "version" INT NOT NULL,
     "position" BIGINT NOT NULL
 );
@@ -18,6 +19,3 @@ CREATE TABLE IF NOT EXISTS Properties (
     "name" TEXT NOT NULL,
     "value" TEXT NOT NULL
 );
-
-INSERT INTO Properties ("name", "value") SELECT 'next_position', 0
-WHERE NOT EXISTS (SELECT 1 FROM Properties WHERE "name" = 'next_position');
