124aec329de4c47a94f0ff7f11d23e901a0036d2
Johan Segerfeldt <johan.segerfeldt@me.com>
2022-05-20 06:16:25 +0200

    Quote all column names

--- Segerfeldt.EventStore.Source/SQLite/schema.sql
+++ Segerfeldt.EventStore.Source/SQLite/schema.sql
@@ -1,19 +1,19 @@
 CREATE TABLE IF NOT EXISTS Entities (
-    id TEXT,
-    type TEXT,
-    version INT,
+    "id" TEXT NOT NULL,
+    "type" TEXT , --NOT NULL,
+    "version" INT NOT NULL,
 
-    PRIMARY KEY (id)
+    PRIMARY KEY ("id")
 );
 
 CREATE TABLE IF NOT EXISTS Events (
-    entity TEXT,
-    name TEXT,
-    details TEXT,
-    actor TEXT,
-    timestamp TIMESTAMP default current_timestamp,
-    version INT,
-    position BIGINT,
+    "entity" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "details" TEXT NOT NULL,
+    "actor" TEXT NOT NULL,
+    "timestamp" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    "version" INT NOT NULL,
+    "position" BIGINT NOT NULL,
 
-    FOREIGN KEY (entity) REFERENCES Entities (id)
+    FOREIGN KEY ("entity") REFERENCES Entities ("id")
 );
