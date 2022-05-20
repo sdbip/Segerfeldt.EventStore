575c10a7bf41696b47fa3c263ec9d3d06f85a1c4
Johan Segerfeldt <johan.segerfeldt@me.com>
2022-05-20 06:14:38 +0200

    Add Source tests for PostgreSQL

---
+++ Tests/Segerfeldt.EventStore.Source.Tests/PostgreSQLPublishingTests.cs
@@ -0,0 +1,123 @@
+using Moq;
+
+using Npgsql;
+
+using NUnit.Framework;
+
+namespace Segerfeldt.EventStore.Source.Tests;
+
+public class PostgreSQLPublishingTests
+{
+    private NpgsqlConnection connection = null!;
+    private EventPublisher publisher = null!;
+
+    [SetUp]
+    public void Setup()
+    {
+        connection = new NpgsqlConnection("Server=localhost;Database=es_test;");
+        var connectionPool = new SingletonConnectionPool(connection);
+        publisher = new EventPublisher(connectionPool);
+        PostgreSQL.Schema.CreateIfMissing(connection);
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        connection.Open();
+        try
+        {
+            connection.CreateCommand("DELETE FROM Events; DELETE FROM Entities;").ExecuteNonQuery();
+        }
+        finally
+        {
+            connection.Close();
+        }
+    }
+
+    [Test]
+    public void CanPublishSingleEvent()
+    {
+        publisher.Publish(new EntityId("an-entity"), new EntityType("a-type"), new UnpublishedEvent("an-event", new{Meaning = 42}), "johan");
+
+        connection.Open();
+        using var reader = connection.CreateCommand("SELECT * FROM Events").ExecuteReader();
+        reader.Read();
+
+        Assert.That(new
+        {
+            Entity = reader["entity"],
+            Name = reader["name"],
+            Details = reader["details"],
+            Version = reader["version"],
+            Position = reader["position"]
+        }, Is.EqualTo(new
+        {
+            Entity = (object) "an-entity",
+            Name = (object) "an-event",
+            Details = (object) @"{""meaning"":42}",
+            Version = (object) 0,
+            Position = (object) 0L
+        }));
+        connection.Close();
+    }
+
+    [Test]
+    public void CanPublishChanges()
+    {
+        var entity = new Mock<IEntity>();
+        entity.Setup(e => e.Id).Returns(new EntityId("an-entity"));
+        entity.Setup(e => e.Type).Returns(new EntityType("a-type"));
+        entity.Setup(e => e.Version).Returns(EntityVersion.New);
+        entity.Setup(e => e.UnpublishedEvents).Returns(new []{new UnpublishedEvent("an-event", new{Meaning = 42})});
+        publisher.PublishChanges(entity.Object, "johan");
+
+        connection.Open();
+        using var reader = connection.CreateCommand("SELECT * FROM Events").ExecuteReader();
+        reader.Read();
+
+        Assert.That(new
+        {
+            Entity = reader["entity"],
+            Name = reader["name"],
+            Details = reader["details"],
+            Version = reader["version"],
+            Position = reader["position"]
+        }, Is.EqualTo(new
+        {
+            Entity = (object) "an-entity",
+            Name = (object) "an-event",
+            Details = (object) @"{""meaning"":42}",
+            Version = (object) 0,
+            Position = (object) 0L
+        }));
+        connection.Close();
+    }
+
+    [Test]
+    public void CannotPublishChangesIfRemoteUpdated()
+    {
+        GivenEntity();
+
+        var entity = new Mock<IEntity>();
+        entity.Setup(e => e.Id).Returns(new EntityId("an-entity"));
+        entity.Setup(e => e.Version).Returns(EntityVersion.Of(2));
+        entity.Setup(e => e.UnpublishedEvents).Returns(new []{new UnpublishedEvent("an-event", new{})});
+
+        Assert.That(async () => await publisher.PublishChangesAsync(entity.Object, "johan"), Throws.Exception);
+    }
+
+    private void GivenEntity()
+    {
+        connection.Open();
+        try
+        {
+            connection
+                .CreateCommand("INSERT INTO Entities (id, type, version) VALUES ('an-entity', 'a-type', 3)")
+                .ExecuteNonQuery();
+        }
+        finally
+        {
+            connection.Close();
+        }
+    }
+}
---
+++ Tests/Segerfeldt.EventStore.Source.Tests/PostgreSQLReconstitutionTests.cs
@@ -0,0 +1,245 @@
+using Npgsql;
+
+using NUnit.Framework;
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace Segerfeldt.EventStore.Source.Tests;
+
+public class PostgreSQLReconstitutionTests
+{
+    private NpgsqlConnection connection = null!;
+    private EntityStore store = null!;
+
+    [SetUp]
+    public void Setup()
+    {
+        connection = new NpgsqlConnection("Server=localhost;Database=es_test;");
+        var connectionPool = new SingletonConnectionPool(connection);
+        store = new EntityStore(connectionPool);
+
+        PostgreSQL.Schema.CreateIfMissing(connection);
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        connection.Open();
+        connection.CreateCommand("DELETE FROM Events; DELETE FROM Entities;").ExecuteNonQuery();
+        connection.Close();
+    }
+
+    [Test]
+    public void ReconstitutesEntities()
+    {
+        connection.Open();
+        try
+        {
+            GivenEntity("an-entity", "a-type", 3);
+        }
+        finally
+        {
+            connection.Close();
+        }
+
+        var entity = store.Reconstitute<MyEntity>(new EntityId("an-entity"), new EntityType("a-type"));
+
+        Assert.That(entity, Is.Not.Null);
+        Assert.That(entity?.Version, Is.EqualTo(EntityVersion.Of(3)));
+    }
+
+    [Test]
+    public void ReturnsNullIfNoEntity()
+    {
+        var entity = store.Reconstitute<MyEntity>(new EntityId("an-entity"), new EntityType("a-type"));
+
+        Assert.That(entity, Is.Null);
+    }
+
+    [Test]
+    public void ReplaysEvent()
+    {
+        connection.Open();
+        try
+        {
+            GivenEntity("an-entity", "a-type");
+            GivenEvent("an-entity", "an-event", @"{""meaning"":42}");
+        }
+        finally
+        {
+            connection.Close();
+        }
+
+        var entity = store.Reconstitute<MyEntity>(new EntityId("an-entity"), new EntityType("a-type"));
+
+        Assert.That(entity?.ReplayedEvents, Is.Not.Null);
+        Assert.That(entity?.ReplayedEvents?.Select(e => new
+            {
+                e.Name,
+                e.Details
+            }),
+            Is.EquivalentTo(new[] { new
+            {
+                Name = "an-event",
+                Details = @"{""meaning"":42}"
+            } }));
+    }
+
+    [Test]
+    public void ReplaysMultipleEventsInOrder()
+    {
+        connection.Open();
+        try
+        {
+            GivenEntity("an-entity", "a-type");
+            GivenEvent("an-entity", "first-event", version: 1);
+            GivenEvent("an-entity", "third-event", version: 3);
+            GivenEvent("an-entity", "second-event", version: 2);
+        }
+        finally
+        {
+            connection.Close();
+        }
+
+        var entity = store.Reconstitute<MyEntity>(new EntityId("an-entity"), new EntityType("a-type"));
+
+        Assert.That(entity?.ReplayedEvents, Is.Not.Null);
+
+        var replayedEvents = entity!.ReplayedEvents!.ToList();
+        Assert.That(replayedEvents[0].Name, Is.EqualTo("first-event"));
+        Assert.That(replayedEvents[1].Name, Is.EqualTo("second-event"));
+        Assert.That(replayedEvents[2].Name, Is.EqualTo("third-event"));
+    }
+
+    [Test]
+    public void CanReadHistoryOnly()
+    {
+        var timestamp = new DateTimeOffset(2021, 08, 12, 17, 22, 35, TimeSpan.Zero);
+        connection.Open();
+        try
+        {
+            GivenEntity("an-entity", "a-type");
+            GivenEvent("an-entity", "first-event", "johan", timestamp);
+        }
+        finally
+        {
+            connection.Close();
+        }
+
+        Assume.That(timestamp.Ticks, Is.EqualTo(637643857550000000L));
+
+        var history = store.GetHistory(new EntityId("an-entity"));
+
+        Assert.That(history, Is.Not.Null);
+
+        var replayedEvents = history!.Events.ToList();
+        Assert.That(replayedEvents[0].Actor, Is.EqualTo("johan"));
+        Assert.That(replayedEvents[0].Timestamp, Is.EqualTo(timestamp));
+    }
+
+    [Test]
+    public void ReadsHistoryInOrder()
+    {
+        connection.Open();
+        try
+        {
+            GivenEntity("an-entity", "a-type");
+            GivenEvent("an-entity", "first-event", version: 1);
+            GivenEvent("an-entity", "third-event", version: 3);
+            GivenEvent("an-entity", "second-event", version: 2);
+        }
+        finally
+        {
+            connection.Close();
+        }
+
+        var history = store.GetHistory(new EntityId("an-entity"));
+
+        Assert.That(history, Is.Not.Null);
+
+        var replayedEvents = history!.Events.ToList();
+        Assert.That(replayedEvents[0].Name, Is.EqualTo("first-event"));
+        Assert.That(replayedEvents[1].Name, Is.EqualTo("second-event"));
+        Assert.That(replayedEvents[2].Name, Is.EqualTo("third-event"));
+    }
+
+    [Test]
+    public void ReturnsTimestampsAsUTC()
+    {
+        connection.Open();
+        try
+        {
+            GivenEntity("an-entity", "a-type");
+            GivenEvent("an-entity", "an-event", @"{""meaning"":42}");
+        }
+        finally
+        {
+            connection.Close();
+        }
+
+        var entity = store.Reconstitute<MyEntity>(new EntityId("an-entity"), new EntityType("a-type"));
+
+        Assert.That(entity?.ReplayedEvents, Is.Not.Null);
+        Assert.That(entity?.ReplayedEvents?.First().Timestamp - DateTimeOffset.UtcNow, Is.LessThan(TimeSpan.FromSeconds(1)));
+        Assert.That(entity?.ReplayedEvents?.First().Timestamp.Offset, Is.EqualTo(TimeSpan.Zero));
+    }
+
+    private void GivenEntity(string entityId, string entityType, int version = 1)
+    {
+        var command = connection.CreateCommand("INSERT INTO Entities (id, type, version) VALUES (@entityId, @entityType, @version)");
+        command.AddParameter("@entityId", entityId);
+        command.AddParameter("@entityType", entityType);
+        command.AddParameter("@version", version);
+        command.ExecuteNonQuery();
+    }
+
+    private void GivenEvent(string entityId, string eventName, string actor, DateTimeOffset timestamp)
+    {
+        string commandText =
+            @"INSERT INTO Events (entity, name, details, actor, timestamp, version, position)
+                    VALUES (@entityId, @eventName, '{}', @actor, @timestamp, 1, 1)";
+        var command = connection.CreateCommand(commandText);
+        command.AddParameter("@entityId", entityId);
+        command.AddParameter("@eventName", eventName);
+        command.AddParameter("@actor", actor);
+        command.AddParameter("@timestamp", timestamp);
+        command.ExecuteNonQuery();
+    }
+
+    private void GivenEvent(string entityId, string eventName, string details = "{}", int version = 1)
+    {
+        var command = connection.CreateCommand(
+            @"INSERT INTO Events (entity, name, details, actor, version, position)
+                    VALUES (@entityId, @eventName, @details, 'test', @version, 1)");
+        command.AddParameter("@entityId", entityId);
+        command.AddParameter("@eventName", eventName);
+        command.AddParameter("@details", details);
+        command.AddParameter("@version", version);
+        command.ExecuteNonQuery();
+    }
+
+    // ReSharper disable once ClassNeverInstantiated.Local
+    private class MyEntity : IEntity
+    {
+        public EntityId Id { get; }
+        public EntityVersion Version { get; }
+        public EntityType Type => new("MyEntity");
+        public IEnumerable<UnpublishedEvent> UnpublishedEvents => ImmutableList<UnpublishedEvent>.Empty;
+
+        public IEnumerable<PublishedEvent>? ReplayedEvents { get; private set; }
+
+        public MyEntity(EntityId id, EntityVersion version)
+        {
+            Id = id;
+            Version = version;
+        }
+
+        public void ReplayEvents(IEnumerable<PublishedEvent> events)
+        {
+            ReplayedEvents = events;
+        }
+    }
+}
