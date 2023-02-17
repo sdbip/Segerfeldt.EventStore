70765b14365ef461337e05a9f965ad3b000ced34
Johan Segerfeldt <johan.segerfeldt@me.com>
2023-02-17 15:53:37 +0100

    Make CommandResult<T> errors more accessible

--- Segerfeldt.EventStore.Source/CommandAPI/CommandResult.cs
+++ Segerfeldt.EventStore.Source/CommandAPI/CommandResult.cs
@@ -63,22 +63,38 @@ public class CommandResult : ICommandResult
 
 public class CommandResult<T> : ICommandResult
 {
+    private readonly T? value;
+
     public int StatusCode { get; }
-    public T? Value { get; }
     public object? Content { get; }
+    public T Value { get => GetValue(); }
 
     private CommandResult(int statusCode, T? value, object? content)
     {
+        this.value = value;
         StatusCode = statusCode;
         Content = content;
-        Value = value;
     }
 
-    internal static CommandResult<T> Ok(T value) => new(StatusCodes.Status200OK, value, value);
+    public static CommandResult<T> Ok(T value) => new(StatusCodes.Status200OK, value, value);
+
+    public static CommandResult<T> BadRequest(object error) => new(StatusCodes.Status400BadRequest, default, error);
+    public static CommandResult<T> NotFound(object error) => new(StatusCodes.Status404NotFound, default, error);
+
+    public static CommandResult<T> Unauthorized() => new(StatusCodes.Status401Unauthorized, default, null);
+    public static CommandResult<T> Unauthorized(object error) => new(StatusCodes.Status401Unauthorized, default, error);
+    public static CommandResult<T> Forbidden() => new(StatusCodes.Status403Forbidden, default, null);
+    public static CommandResult<T> Forbidden(object error) => new(StatusCodes.Status403Forbidden, default, error);
 
     public static CommandResult<T> Error(ICommandResult other)
     {
         if (!other.IsError()) throw new Exception("Successful result used as error");
         return new CommandResult<T>(other.StatusCode, default, other.Content);
+    }
+
+    private T GetValue()
+    {
+        if (value is null) throw new Exception("Attempt to get value from failed result.");
+        return value!;
     }
 }
