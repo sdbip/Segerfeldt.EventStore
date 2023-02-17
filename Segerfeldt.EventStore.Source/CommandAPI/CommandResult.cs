4bd6fd241db1c6908188adf78e0931666b4336ec
Johan Segerfeldt <johan.segerfeldt@me.com>
2023-02-17 07:29:39 +0100

    Add ICommandResult.SameError() and SameErrorFor<T>()

--- Segerfeldt.EventStore.Source/CommandAPI/CommandResult.cs
+++ Segerfeldt.EventStore.Source/CommandAPI/CommandResult.cs
@@ -1,66 +1,84 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
+using System;
+
 namespace Segerfeldt.EventStore.Source.CommandAPI;
 
 public interface ICommandResult
 {
-    int StatusCode { get; }
-    object? Message { get; }
-}
-
-internal static class CommandResultStatus
-{
-    public static ActionResult ActionResult(this ICommandResult handlerResult) =>
-        handlerResult.Message is null
-            ? new StatusCodeResult(handlerResult.StatusCode)
-            : new ObjectResult(handlerResult.Message) { StatusCode = handlerResult.StatusCode };
-}
-
-public struct ResultData
-{
     public int StatusCode { get; }
-    public object? Message { get; }
+    public object? Content { get; }
+}
 
-    public ResultData(int statusCode, object? message)
+public static class CommandResultExtension
+{
+    public static ActionResult ActionResult(this ICommandResult result) =>
+        result.Content is null
+            ? new StatusCodeResult(result.StatusCode)
+            : new ObjectResult(result.Content) { StatusCode = result.StatusCode };
+
+    public static bool IsError(this ICommandResult commandResult) => commandResult.StatusCode / 100 != 2;
+
+    public static CommandResult SameError(this ICommandResult commandResult)
     {
-        Message = message;
-        StatusCode = statusCode;
+        if (!commandResult.IsError()) throw new Exception("Successful result used as error");
+        return CommandResult.Error(commandResult);
+    }
+
+    public static CommandResult<TOther> SameErrorFor<TOther>(this ICommandResult commandResult)
+    {
+        return CommandResult<TOther>.Error(commandResult);
     }
 }
 
 public class CommandResult : ICommandResult
 {
-    private readonly ResultData data;
+    public int StatusCode { get; }
+    public object? Content { get; }
 
-    public int StatusCode { get => data.StatusCode; }
-    public object? Message { get => data.Message; }
+    private CommandResult(int statusCode, object? content)
+    {
+        StatusCode = statusCode;
+        Content = content;
+    }
 
-    private CommandResult(ResultData data) => this.data = data;
+    public static CommandResult Ok() => new(StatusCodes.Status204NoContent, null);
+    public static CommandResult<T> Ok<T>(T value) => CommandResult<T>.Ok(value);
 
-    public static CommandResult Ok() => new(new ResultData(StatusCodes.Status204NoContent, null));
-    public static CommandResult<T> Ok<T>(T value) => new(new ResultData(StatusCodes.Status200OK, value));
+    public static CommandResult BadRequest(object error) => new(StatusCodes.Status400BadRequest, error);
+    public static CommandResult NotFound(object error) => new(StatusCodes.Status404NotFound, error);
 
-    public static ResultData BadRequest(object error) => Error(StatusCodes.Status400BadRequest, error);
-    public static ResultData NotFound(object error) => Error(StatusCodes.Status404NotFound, error);
+    public static CommandResult Unauthorized() => new(StatusCodes.Status401Unauthorized, null);
+    public static CommandResult Unauthorized(object error) => new(StatusCodes.Status401Unauthorized, error);
+    public static CommandResult Forbidden() => new(StatusCodes.Status403Forbidden, null);
+    public static CommandResult Forbidden(object error) => new(StatusCodes.Status403Forbidden, error);
 
-    public static ResultData Unauthorized() => Error(StatusCodes.Status401Unauthorized);
-    public static ResultData Unauthorized(object error) => Error(StatusCodes.Status401Unauthorized, error);
-    public static ResultData Forbidden() => Error(StatusCodes.Status403Forbidden);
-    public static ResultData Forbidden(object error) => Error(StatusCodes.Status403Forbidden, error);
-
-    public static ResultData Error(int statusCode, object? error = null) => new(statusCode, error);
-    public static implicit operator CommandResult(ResultData data) => new(data);
+    internal static CommandResult Error(ICommandResult other)
+    {
+        if (!other.IsError()) throw new Exception("Successful result used as error");
+        return new CommandResult(other.StatusCode, other.Content);
+    }
 }
 
 public class CommandResult<T> : ICommandResult
 {
-    private readonly ResultData data;
+    public int StatusCode { get; }
+    public T? Value { get; }
+    public object? Content { get; }
 
-    public int StatusCode { get => data.StatusCode; }
-    public object? Message { get => data.Message; }
+    private CommandResult(int statusCode, T? value, object? content)
+    {
+        StatusCode = statusCode;
+        Content = content;
+        Value = value;
+    }
 
-    public CommandResult(ResultData data) => this.data = data;
+    internal static CommandResult<T> Ok(T value) => new(StatusCodes.Status200OK, value, value);
 
-    public static implicit operator CommandResult<T>(ResultData data) => new(data);
+    public static CommandResult<T> Error(ICommandResult other)
+    {
+        if (!other.IsError()) throw new Exception("Successful result used as error");
+        return new CommandResult<T>(other.StatusCode, default, other.Content);
+    }
 }
---
+++ Tests/Segerfeldt.EventStore.Source.Tests/CommandResultTests.cs
@@ -0,0 +1,79 @@
+using Microsoft.AspNetCore.Mvc;
+
+using NUnit.Framework;
+
+using Segerfeldt.EventStore.Source.CommandAPI;
+
+using static Segerfeldt.EventStore.Source.CommandAPI.CommandResult;
+
+using Require = NUnit.Framework.Assert;
+
+namespace Segerfeldt.EventStore.Source.Tests;
+
+public class CommandResultTests
+{
+    [Test]
+    public void NoValue_ConvertsToActionResultWithoutMessage()
+    {
+        var result = Ok();
+        var actionResult = result.ActionResult();
+
+        Assert.That(result.Content, Is.Null);
+        Assert.That(actionResult, Is.TypeOf<StatusCodeResult>());
+        Assert.That((actionResult as StatusCodeResult)?.StatusCode, Is.EqualTo(204));
+    }
+
+    [Test]
+    public void Value_ConvertsToActionResultWithMessage()
+    {
+        var result = Ok(new {});
+        var actionResult = result.ActionResult();
+
+        Assert.That(result.Value, Is.EqualTo(new {}));
+        Assert.That(actionResult, Is.TypeOf<ObjectResult>());
+        Assert.That((actionResult as ObjectResult)?.StatusCode, Is.EqualTo(200));
+        Assert.That((actionResult as ObjectResult)?.Value, Is.EqualTo(new {}));
+    }
+
+    [Test]
+    public void Error_ConvertsToActionResultWithMessage()
+    {
+        var result = BadRequest("sample error message");
+        var actionResult = result.ActionResult();
+
+        Assert.That(result.Content, Is.EqualTo("sample error message"));
+        Assert.That(actionResult, Is.TypeOf<ObjectResult>());
+        Assert.That((actionResult as ObjectResult)?.StatusCode, Is.EqualTo(400));
+        Assert.That((actionResult as ObjectResult)?.Value, Is.EqualTo("sample error message"));
+    }
+
+    [Test]
+    public void UntypedError_ChainsToTypedError()
+    {
+        var initial = BadRequest("sample error message");
+        var result = initial.SameErrorFor<string>();
+
+        Assert.That(result.IsError(), Is.True);
+        Assert.That(new {result.StatusCode, result.Content, result.Value}, Is.EqualTo(new {initial.StatusCode, initial.Content, Value = null as string}));
+    }
+
+    [Test]
+    public void TypedError_ChainsToOtherType()
+    {
+        var initial = BadRequest("sample error message").SameErrorFor<int>();
+        var result = initial.SameErrorFor<string>();
+
+        Assert.That(result.IsError(), Is.True);
+        Assert.That(new {result.StatusCode, result.Content, result.Value}, Is.EqualTo(new {initial.StatusCode, initial.Content, Value = null as string}));
+    }
+
+    [Test]
+    public void TypedError_ChainsToUntypedError()
+    {
+        var initial = BadRequest("sample error message").SameErrorFor<int>();
+        var result = initial.SameError();
+
+        Assert.That(result.IsError(), Is.True);
+        Assert.That(new {result.StatusCode, result.Content}, Is.EqualTo(new {initial.StatusCode, initial.Content}));
+    }
+}
