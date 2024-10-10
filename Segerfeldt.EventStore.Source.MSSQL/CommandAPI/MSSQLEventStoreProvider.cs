3af4f1efceea32eb13f79a1d86b168cf09f1c453
Johan Segerfeldt <johan.segerfeldt@me.com>
2024-10-10 16:33:09 +0200

    Add API to connect CommandAPI to a SQL Server database

---
+++ Segerfeldt.EventStore.Source.MSSQL/CommandAPI/MSSQLEventStoreProvider.cs
@@ -0,0 +1,15 @@
+using Segerfeldt.EventStore.Source.CommandAPI;
+
+using System;
+using System.Data.Common;
+using System.Data.SqlClient;
+
+namespace Segerfeldt.EventStore.Source.MSSQL.CommandAPI;
+
+/// <summary>EventStore provider for a MS SQL Server database that automatically adds the EventStore schema if missing</summary>
+/// <param name="connectionString">the connection-string to access the database</param>
+public class MSSQLEventStoreProvider(string connectionString) : IEventStoreProvider
+{
+    public void PrepareDatabase(IServiceProvider _) { Schema.CreateIfMissing(CreateConnection()); }
+    public DbConnection CreateConnection() => new SqlConnection(connectionString);
+}
