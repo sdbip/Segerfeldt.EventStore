856ef447d75c822cb718af0faf5dfba368605a93
Johan Segerfeldt <johan.segerfeldt@me.com>
2024-10-14 13:59:18 +0200

    Add documentation to the new API

--- Segerfeldt.EventStore.Source.NUnit/HttpClientExtension.cs
+++ Segerfeldt.EventStore.Source.NUnit/HttpClientExtension.cs
@@ -10,17 +10,33 @@ public static class HttpClientExtension
 {
     private static readonly JsonSerializerOptions CamelCase = new() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
 
-    public static async Task<HttpResponseMessage> SendPostCommand(this HttpClient client, string relativeURL, object command) =>
-        await client.SendCommand(HttpMethod.Post, relativeURL, command);
+    /// <summary>Send a POST request with a serialized command to the application</summary>
+    /// <param name="client">The client to send the command with</param>
+    /// <param name="path">The URL path to query (e.g. "/Entity/id/property"</param>
+    /// <param name="command">The command DTO to send</param>
+    /// <returns>The response from the command handler (or 404 NOT FOUND if the command handler is not set up correctly)</returns>
+    public static async Task<HttpResponseMessage> SendPostCommand(this HttpClient client, string path, object command) =>
+        await client.SendCommand(HttpMethod.Post, path, command);
 
-    public static async Task<HttpResponseMessage> SendDeleteCommand(this HttpClient client, string relativeURL, object command) =>
-        await client.SendCommand(HttpMethod.Delete, relativeURL, command);
+    /// <summary>Send a DELETE request with a serialized command to the application</summary>
+    /// <param name="client">The client to send the command with</param>
+    /// <param name="path">The URL path to query (e.g. "/Entity/id/property"</param>
+    /// <param name="command">The command DTO to send</param>
+    /// <returns>The response from the command handler (or 404 NOT FOUND if the command handler is not set up correctly)</returns>
+    public static async Task<HttpResponseMessage> SendDeleteCommand(this HttpClient client, string path, object command) =>
+        await client.SendCommand(HttpMethod.Delete, path, command);
 
-    public static async Task<HttpResponseMessage> SendCommand(this HttpClient client, HttpMethod method, string relativeURL, object command)
+    /// <summary>Send a request with a serialized command to the application</summary>
+    /// <param name="client">The client to send the command with</param>
+    /// <param name="method">The method/verb to use in the request</param>
+    /// <param name="path">The URL path to query (e.g. "/Entity/id/property"</param>
+    /// <param name="command">The command DTO to send</param>
+    /// <returns>The response from the command handler (or 404 NOT FOUND if the command handler is not set up correctly)</returns>
+    public static async Task<HttpResponseMessage> SendCommand(this HttpClient client, HttpMethod method, string path, object command)
     {
         ArgumentNullException.ThrowIfNull(client, nameof(client));
 
-        var request = new HttpRequestMessage(method, new Uri(relativeURL, UriKind.Relative))
+        var request = new HttpRequestMessage(method, new Uri(path, UriKind.Relative))
         {
             Content = JsonContent.Create(command, options: CamelCase)
         };
