529f8df996be68db44ade809a2fd659990c62c9f
Johan Segerfeldt <johan.segerfeldt@me.com>
2024-10-16 12:40:07 +0200

    Move class to a separate file

--- Apps/SourceWebApplication/Domain/EmailAddressAvailability.cs
+++ Apps/SourceWebApplication/Domain/EmailAddressAvailability.cs
@@ -44,22 +44,3 @@ internal sealed class EmailAddressAvailability(EntityId id, EntityVersion versio
 
     internal record EmailAddressDetails(string EmailAddress);
 }
-
-internal sealed class EmailAddress : ValueObject<EmailAddress>
-{
-    private readonly string value;
-
-    private EmailAddress(string value) => this.value = value;
-
-    public static EmailAddress Of(string value)
-    {
-        ArgumentException.ThrowIfNullOrEmpty($"invalid email address {value}", nameof(value));
-        return new EmailAddress(value);
-    }
-
-    public static EmailAddress Prevalidated(string value) => new(value);
-
-    public static implicit operator string(EmailAddress value) => value.value;
-
-    protected override IEnumerable<object> GetEqualityComponents() => [value];
-}
