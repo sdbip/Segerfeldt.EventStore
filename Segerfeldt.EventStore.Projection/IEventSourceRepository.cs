7eeea6389c4db30c08edfef33dc863714005e2d0
Johan Segerfeldt <johan.segerfeldt@me.com>
2024-10-29 14:05:15 +0100

    Remove usings

--- Segerfeldt.EventStore.Projection/IEventSourceRepository.cs
+++ Segerfeldt.EventStore.Projection/IEventSourceRepository.cs
@@ -1,9 +1,4 @@
-using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Data.Common;
-
-using Segerfeldt.EventStore.Shared;
 
 namespace Segerfeldt.EventStore.Projection;
 
