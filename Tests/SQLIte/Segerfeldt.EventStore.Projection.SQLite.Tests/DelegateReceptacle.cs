4e37f3e425b1d95ac71fdccc23cb04dd1f733f85
Johan Segerfeldt <johan.segerfeldt@me.com>
2024-10-30 10:39:53 +0100

    Delete async delegates

--- Tests/SQLIte/Segerfeldt.EventStore.Projection.SQLite.Tests/DelegateReceptacle.cs
+++ Tests/SQLIte/Segerfeldt.EventStore.Projection.SQLite.Tests/DelegateReceptacle.cs
@@ -1,28 +1,13 @@
 namespace Segerfeldt.EventStore.Projection.SQLite.Tests;
 
-public delegate Task AsyncProjectionDelegate(Event @event);
 public delegate void ProjectionDelegate(Event @event);
 
 /// <summary><see cref="IReceptacle"/> implementation that uses a delegate</summary>
 public sealed class DelegateReceptacle : IReceptacle
 {
-    private readonly Delegate @delegate;
+    private readonly ProjectionDelegate @delegate;
 
     public IEnumerable<string> AcceptedEvents { get; }
-
-    /// <summary>Initialize a new <see cref="DelegateReceptacle"/></summary>
-    /// <param name="delegate">the delegate to call when events are notified</param>
-    /// <param name="handledEvents">the events this delegate handles</param>
-    public DelegateReceptacle(AsyncProjectionDelegate @delegate, params string[] handledEvents) : this(@delegate, (IEnumerable<string>)handledEvents) { }
-
-    /// <summary>Initialize a new <see cref="DelegateReceptacle"/></summary>
-    /// <param name="delegate">the delegate to call when events are notified</param>
-    /// <param name="handledEvents">the events this delegate handles</param>
-    public DelegateReceptacle(AsyncProjectionDelegate @delegate, IEnumerable<string> handledEvents)
-    {
-        AcceptedEvents = handledEvents;
-        this.@delegate = @delegate;
-    }
 
     /// <summary>Initialize a new <see cref="DelegateReceptacle"/></summary>
     /// <param name="delegate">the delegate to call when events are notified</param>
@@ -38,5 +23,5 @@ public sealed class DelegateReceptacle : IReceptacle
         this.@delegate = @delegate;
     }
 
-    public void Update(Event @event) => @delegate.DynamicInvoke(@event);
+    public void Update(Event @event) => @delegate(@event);
 }
--- Tests/Segerfeldt.EventStore.Projection.Tests/DelegateReceptacle.cs
+++ Tests/Segerfeldt.EventStore.Projection.Tests/DelegateReceptacle.cs
@@ -1,32 +1,15 @@
-using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace Segerfeldt.EventStore.Projection.Tests;
 
-public delegate Task AsyncProjectionDelegate(Event @event);
 public delegate void ProjectionDelegate(Event @event);
 
 /// <summary><see cref="IReceptacle"/> implementation that uses a delegate</summary>
 public sealed class DelegateReceptacle : IReceptacle
 {
-    private readonly Delegate @delegate;
+    private readonly ProjectionDelegate @delegate;
 
     public IEnumerable<string> AcceptedEvents { get; }
-
-    /// <summary>Initialize a new <see cref="DelegateReceptacle"/></summary>
-    /// <param name="delegate">the delegate to call when events are notified</param>
-    /// <param name="handledEvents">the events this delegate handles</param>
-    public DelegateReceptacle(AsyncProjectionDelegate @delegate, params string[] handledEvents) : this(@delegate, (IEnumerable<string>)handledEvents) { }
-
-    /// <summary>Initialize a new <see cref="DelegateReceptacle"/></summary>
-    /// <param name="delegate">the delegate to call when events are notified</param>
-    /// <param name="handledEvents">the events this delegate handles</param>
-    public DelegateReceptacle(AsyncProjectionDelegate @delegate, IEnumerable<string> handledEvents)
-    {
-        AcceptedEvents = handledEvents;
-        this.@delegate = @delegate;
-    }
 
     /// <summary>Initialize a new <see cref="DelegateReceptacle"/></summary>
     /// <param name="delegate">the delegate to call when events are notified</param>
@@ -42,5 +25,5 @@ public sealed class DelegateReceptacle : IReceptacle
         this.@delegate = @delegate;
     }
 
-    public void Update(Event @event) => @delegate.DynamicInvoke(@event);
+    public void Update(Event @event) => @delegate(@event);
 }
