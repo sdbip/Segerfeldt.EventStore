2b4f63468d532dc190cd455407df0a939993022d
Johan Segerfeldt <johan.segerfeldt@me.com>
2024-11-13 03:54:46 +0100

    Change type to DbConnection to make it awaitable
    Cannot use SqliteConnection as one test uses InMemoryConnection (which is a necessary wrapper that prevents the connection from closing).

--- SQLite/Segerfeldt.EventStore.Projection.SQLite/Hosting/SQLiteEventSourceRepository.cs
+++ SQLite/Segerfeldt.EventStore.Projection.SQLite/Hosting/SQLiteEventSourceRepository.cs
@@ -6,14 +6,11 @@ using System.Threading.Tasks;
 
 namespace Segerfeldt.EventStore.Projection.SQLite.Hosting;
 
-public sealed class SQLiteEventSourceRepository(IDbConnection connection) : IEventSourceRepository
+public sealed class SQLiteEventSourceRepository(DbConnection connection) : IEventSourceRepository
 {
-    private readonly IDbConnection connection = connection;
-
-    public Task<IEnumerable<Event>> GetEventsAsync(long afterPosition, int maxCount) => Task.FromResult(GetEvents(afterPosition, maxCount));
-    private IEnumerable<Event> GetEvents(long afterPosition, int maxCount)
+    public async Task<IEnumerable<Event>> GetEventsAsync(long afterPosition, int maxCount)
     {
-        using var command = connection.CreateCommand("""
+        await using var command = connection.CreateCommand("""
             SELECT Events.*, Entities.type AS entity_type FROM Events
                 JOIN Entities ON Entities.id = Events.entity_id
                 WHERE position > @position
@@ -22,8 +19,8 @@ public sealed class SQLiteEventSourceRepository(IDbConnection connection) : IEve
         command.AddParameter("@position", afterPosition);
         command.AddParameter("@maxCount", maxCount);
 
-        connection.Open();
-        try { return command.ExecuteReader().AllRowsAs(ReadEvent); }
+        await connection.OpenAsync();
+        try { return (await command.ExecuteReaderAsync()).AllRowsAs(ReadEvent); }
         catch (DbException) { return []; } // No connection => no events.
         finally { connection.Close(); }
     }
